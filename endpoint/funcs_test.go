19bf925866fc6d254bb60c8c0c24e765b06b3c17
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2019-04-19 22:01:17 +0200

    use dep properly

--- endpoint/funcs_test.go
+++ endpoint/funcs_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestAdd(t *testing.T) {
-	total := Add(5, 5)
-	if total != 10 {
+	total := Add(5, 6)
+	if total != 11 {
 		t.Errorf("Sum was incorrect, got: %d, want: %d.", total, 10)
 	}
 }
