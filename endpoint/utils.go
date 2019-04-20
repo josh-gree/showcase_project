0b6c575811d9bb01f863378016ac0f6b776451e6
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2019-04-20 16:35:00 +0200

    add random wait time and different random groups to endpoint

--- endpoint/utils.go
+++ endpoint/utils.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+	"math"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type NormParams struct {
+	mu    float64
+	sigma float64
+}
+
+type Group struct {
+	id     int
+	params NormParams
+}
+
+var groups = []Group{
+	{id: 1, params: NormParams{mu: 1.2, sigma: 0.1}},
+	{id: 2, params: NormParams{mu: 9.4, sigma: 2.3}},
+	{id: 3, params: NormParams{mu: 5.1, sigma: 0.3}},
+}
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
@@ -13,5 +32,22 @@ func SetupRouter() *gin.Engine {
 }
 
 func RouteHandler(c *gin.Context) {
+	// choose a random group
+	seed := time.Now().UTC().UnixNano()
+	grp := groups[rand.Intn(len(groups))]
+	params := grp.params
+
+	// get wait duration and wait
+	duration := RandomDuration(params, seed)
+	time.Sleep(duration)
+
+	// return
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
+}
+
+func RandomDuration(params NormParams, seed int64) time.Duration {
+	rand.Seed(seed)
+	normal_draw := math.Abs(rand.NormFloat64()*params.sigma + params.mu)
+	duration := time.Duration(normal_draw*1000) * time.Millisecond
+	return duration
 }
