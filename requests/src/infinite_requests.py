726b2ebdfe2a17c99f3785f4136cbba2e78b69ee
josh-gree <joshuadouglasgreenhalgh@gmail.com>
2019-04-20 23:05:51 +0200

    make script take cli args for task limit and url

--- requests/src/infinite_requests.py
+++ requests/src/infinite_requests.py
@@ -3,28 +3,38 @@ Script to make infinite requests to endpoint
 """
 
 import asyncio
+import click
 from aiohttp import ClientSession, TCPConnector
 from pypeln import asyncio_task as aio
 from itertools import repeat
 
-limit = 10
-urls = repeat("http://endpoint:80/")
-
 
 async def fetch(url, session):
-    '''
+    """
     Make an async request
-    '''
+    """
     async with session.get(url) as response:
         await response.read()
         print(response._body)
 
 
-aio.each(
-    fetch,
-    urls,
-    workers=limit,
-    on_start=lambda: ClientSession(connector=TCPConnector(limit=None)),
-    on_done=lambda _status, session: session.close(),
-    run=True,
-)
+@click.command()
+@click.option("--task-limit", default=100, help="Limit of concurrent tasks")
+@click.option("--url", default="http://endpoint:80/", help="url to send requests")
+def main(task_limit, url):
+    """
+    CLI main function
+    """
+    urls = repeat(url)
+    aio.each(
+        fetch,
+        urls,
+        workers=task_limit,
+        on_start=lambda: ClientSession(connector=TCPConnector(limit=None)),
+        on_done=lambda _status, session: session.close(),
+        run=True,
+    )
+
+
+if __name__ == "__main__":
+    main()
